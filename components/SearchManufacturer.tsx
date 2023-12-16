b9297adba8bf815722541fb8c52f62279c682781
Sumit Mahat <mahatsumit5@gmail.com>
2023-12-16 22:01:34 +1100

    searech filter

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,12 +1,24 @@
 "use client";
 import { SearchManufacturerProps } from "@/types";
-import React from "react";
+import React, { useState, Fragment } from "react";
 import Image from "next/image";
 import { Combobox, Transition } from "@headlessui/react";
+import { manufacturers } from "@/constants";
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
+  const filteredManufactureres =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLocaleLowerCase()
+            .replace(/\s+/g, "") //replace empty spacea
+            .includes(query.replace(/\s+/g, "").toLocaleLowerCase())
+        );
+
   return (
     <div className="search-manufacturer">
       <Combobox>
@@ -20,7 +32,36 @@ const SearchManufacturer = ({
               className="ml-4"
             />
           </Combobox.Button>
-          <Combobox.Input className="search-manufacturer__input" />
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Options>
+              {filteredManufactureres.map((item) => (
+                <Combobox.Option
+                  key={item}
+                  className={({
+                    active,
+                  }) => `realtive search-manufacturer__option
+                  ${active ? "bg-primary-blue text-white" : "text-gray-900"}
+                  
+                  `}
+                  value={item}
+                >
+                  {item}
+                </Combobox.Option>
+              ))}
+            </Combobox.Options>
+          </Transition>
         </div>
       </Combobox>
     </div>
