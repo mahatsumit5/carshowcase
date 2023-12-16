8018480d607ea20faba260f0cc82420697f8b9bf
Sumit Mahat <mahatsumit5@gmail.com>
2023-12-16 21:43:17 +1100

    headless ui

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,11 +1,30 @@
+"use client";
 import { SearchManufacturerProps } from "@/types";
 import React from "react";
-
+import Image from "next/image";
+import { Combobox, Transition } from "@headlessui/react";
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
-  return <div>sfasdf</div>;
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px] ">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              alt="car logo"
+              className="ml-4"
+            />
+          </Combobox.Button>
+          <Combobox.Input className="search-manufacturer__input" />
+        </div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;
